45e3e736f4c2b5e8df5b3fc8e4a955ac5d67331b
Ashleylinn <ashleylin1016@gmail.com>
2024-09-26 14:19:39 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Driver {
 	public static void main(String[] args) throws IOException{
-		Polynomial p = new Polynomial();
+	Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3)); 
         
         double[] c1 = {6, -2, 5};
